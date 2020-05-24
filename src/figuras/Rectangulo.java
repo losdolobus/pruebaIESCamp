86c2044c0b5548e39b7168ae6385303902509994
losdolobus <60858754+losdolobus@users.noreply.github.com>
2020-05-24 14:32:24 +0200

    Creacion de la clase 'Rectangulo' y del metodo 'obtenerArea'.

---
+++ src/figuras/Rectangulo.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package figuras;
+
+/**
+ *
+ * @author Iván
+ */
+public class Rectangulo {
+    private double lado1;
+    private double lado2;
+    
+    public Rectangulo (double l1, double l2) {
+        lado1 = l1;
+        lado2 = l2;
+    }
+    public double obtenerArea () {
+        return lado1 * lado2;
+    }
+    public double obtenerPerimetro () {
+        return (lado1 * 2) + (lado2 * 2);
+    }
+}
